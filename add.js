db275553085bc5f0dd11b0d7faadaa60491e46da
Santhosh <r-n.santhosh@siemens.com>
2024-10-01 15:12:53 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,9 @@
+const name = "R N Santhosh";
+const hobbies = ["Watching movies", "Playing Cricket", "Travelling"];
+const birth = {
+    day: 8,
+    month: "August",
+    year: 2003
+};
+
+console.log(`My name is ${name}. I enjoy ${hobbies.join(", ")}. I was born on ${birth.day} ${birth.month}, ${birth.year}.`);
